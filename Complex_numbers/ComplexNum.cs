772212771cab73374425fe8d029783bc047de6a1
TarasRubay <84627325+TarasRubay@users.noreply.github.com>
2021-09-11 20:55:23 +0300

    Add project files.

---
+++ Complex_numbers/ComplexNum.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_numbers
+{
+    public class ComplexNum
+    {
+        readonly double real_part = 0.0;
+        readonly double imaginary_part = 0.0;
+
+
+        public ComplexNum() { }
+        public ComplexNum(double A, double B)
+        {
+            real_part = A;
+            imaginary_part = B;
+        }
+
+        public double Real_part => real_part;
+
+        public double Imaginary_part => imaginary_part;
+
+
+
+        public override bool Equals(object obj)
+        {
+            return obj is ComplexNum num &&
+                   Real_part == num.Real_part &&
+                   Imaginary_part == num.Imaginary_part;
+        }
+
+        public override int GetHashCode() => (this.Real_part + this.Imaginary_part).ToString().GetHashCode();
+        public static bool operator==(ComplexNum a, ComplexNum b){
+            if (a.Imaginary_part == b.Imaginary_part && a.Real_part == b.Real_part) return true;
+            else return false;
+        }
+        public static bool operator!=(ComplexNum first, ComplexNum last){
+            if (first.Imaginary_part != last.Imaginary_part && first.Real_part != last.Real_part) return false;
+            else return true;
+        }
+        public static ComplexNum operator +(ComplexNum first,ComplexNum last)
+        {
+            return new ComplexNum(first.Real_part + last.Real_part,first.Imaginary_part + last.Imaginary_part);
+        }
+        public static ComplexNum operator -(ComplexNum first, ComplexNum last)
+        {
+            return new ComplexNum(first.Real_part - last.Real_part, first.Imaginary_part - last.Imaginary_part);
+        }
+        public static ComplexNum operator *(ComplexNum first, ComplexNum last)
+        {
+            return new ComplexNum((first.Real_part * last.Real_part) - (first.Imaginary_part * last.Imaginary_part),(first.Imaginary_part * last.Real_part) + (first.Real_part * last.Imaginary_part));
+        }
+        public static ComplexNum operator /(ComplexNum first, ComplexNum last)
+        {
+            return new ComplexNum(((first.Real_part * last.Real_part) + (first.Imaginary_part * last.Imaginary_part)) / (Math.Sqrt(last.Real_part) + Math.Sqrt(last.Imaginary_part)),
+                (first.Imaginary_part * last.Real_part) - (first.Real_part * last.Imaginary_part) / (Math.Sqrt(last.Real_part) + Math.Sqrt(last.Imaginary_part)));
+        }
+        public static explicit operator double(ComplexNum CN)
+        {
+            return (double)CN.Real_part + CN.Imaginary_part;
+        }
+        public static explicit operator ComplexNum(double num)
+        {
+            return new ComplexNum(num,0);
+        }
+        public override string ToString()
+        {
+            return $"{Real_part} + {Imaginary_part}i";
+
+            
+        }
+    }
+}
---
+++ Complex_numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNum z = new ComplexNum(1,1);
+            ComplexNum z1;
+            z1 = z - (z * z * z - (ComplexNum)1) / ((ComplexNum)3 * z * z);
+            Console.WriteLine("z1 = z - (z * z * z - 1) / (3 * z * z)");
+            Console.WriteLine($"z1 = {z1}");
+            Console.ReadKey();
+        }
+    }
+}
